bb3f38f2521da6f3298dc7efa3411e0d44035b63
Torsten Kunkel <git@tkunkel.de>
2021-11-21 02:54:33 +0100

    build + proxy conf

--- server/src/services/configuration.service.ts
+++ server/src/services/configuration.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@nestjs/common';
 import {S3Client} from "@aws-sdk/client-s3";
 
-const IAM_USER_KEY = " W7XE58Z1QL3RAHH0L12O";
-const IAM_USER_SECRET = "r85dqpA45UuY7+FYp0GgPQWHGhPHWrfzYuG+NUe+";
+const IAM_USER_KEY = process.env.IAM_USER_KEY;
+const IAM_USER_SECRET = process.env.MINIO_IAM_USER_SECRET;
 
 @Injectable()
 export class ConfigurationService {
