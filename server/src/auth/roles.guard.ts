59a4da22032f2c7169a5236381eddfb7e497ea41
Torsten Kunkel <git@tkunkel.de>
2021-11-26 01:27:45 +0100

    * fixing authorization

--- server/src/auth/roles.guard.ts
+++ server/src/auth/roles.guard.ts
@@ -1,4 +1,4 @@
-import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
+import {Injectable, CanActivate, ExecutionContext, Headers} from '@nestjs/common';
 import {Reflector} from '@nestjs/core';
 import {ROLES_KEY} from "./roles.decorator";
 import {Role} from "./role.enum";
@@ -18,7 +18,11 @@ export class RolesGuard implements CanActivate {
         if (!requiredRoles) {
             return true;
         }
-        const rc = await axios.get(process.env.EAGLE_AUTH_SERVER_URL + '/v1/auth/roles');
+
+        const authHeader = context.switchToHttp().getRequest().headers.authorization;
+        console.log('RolesGuard.authHeader', authHeader);
+
+        const rc = await axios.get(process.env.EAGLE_AUTH_SERVER_URL + '/v1/auth/roles?authHeader='+authHeader);
         const userRoles = rc?.data as string[];
         console.log('requiredRoles', requiredRoles);
         console.log('userRoles', userRoles);
